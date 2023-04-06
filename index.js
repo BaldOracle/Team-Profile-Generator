21f09bfbc281e2bddaa719d0e37d8264ec4dbc93
Mr Milk Man <sdmurphy861@gmail.com>
2023-04-06 09:28:42 -0400

    added href an to index.html

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function createTeam() {
             type: 'list',
             name: 'teamType',
             message: 'Who do you want to add first?',
-            choices: ['Engineer', 'Intern', 'Exit']
+            choices: ['Engineer', 'Intern', 'Create-Team']
          },
       ])
       .then((response) => {
@@ -29,10 +29,77 @@ function createTeam() {
 
          } else if (response.teamType === 'Intern') {
             createIntern();
-         } else if (response.teamType === 'Exit'){
+         } else if (response.teamType === 'Create-Team') {
             console.log(team)
+            buildTeam();
          }
       })
+}
+
+function buildTeam() {
+   fs.writeFileSync("./dist/team.html", putItTogether()) 
+}
+
+function putItTogether() {
+   return `
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+
+    <title>Document</title>
+</head>
+<body>
+
+
+   ${team.map(teamMember => {
+      let role = teamMember.getRole()
+      let email = teamMember.getEmail()
+      let phone = teamMember.getOfficeNumber()
+      let git = teamMember.getGithub()
+      let htmlCard = `
+      <div class="card" style="width: 18rem;">
+         <div class="card-header">
+           Role: ${role}
+         </div>
+         <ul class="list-group list-group-flush">
+           <li class="list-group-item">Name: ${teamMember.getName()}</li>
+           <li class="list-group-item">ID: ${teamMember.getId()}</li>
+           <li class="list-group-item" href=${email}>Email: ${teamMember.getEmail()}</li>
+      `
+      if (role === 'Manager') {
+         return `
+         
+            ${htmlCard}
+           <li class="list-group-item" href=${phone}>Phone Number: ${phone}</li>
+ 
+         </ul>
+       </div>`
+      } else if (role === 'Engineer') {
+         return `
+         ${htmlCard}
+           <li class="list-group-item" href =https://github.com/${git}>Github: ${teamMember.getGithub()}</li>
+ 
+         </ul>
+       </div>
+         `
+
+      } else if (role === 'Intern') {
+         return `
+         ${htmlCard}
+         <li class="list-group-item">A=School: ${teamMember.getSchool()}</li>
+       </ul>
+     </div>
+       `
+      }
+   })}
+   </body>
+</html>
+   `
 }
 
 
@@ -100,13 +167,13 @@ function createEngineer() {
          {
             type: 'input',
             message: 'What is their Github user name?',
-            name: 'gitHubUserName',
+            name: 'getGithub',
          }
       ])
       .then((response) => {
          // Use user feedback for... whatever!!
-      
-         const newEngineer = new Engineer(response.engineerName, response.employeeId, response.email, response.gitHubUserName)
+
+         const newEngineer = new Engineer(response.engineerName, response.employeeId, response.email, response.getGithub)
          console.log(newEngineer)
          team.push(newEngineer)
          createTeam();
@@ -161,6 +228,8 @@ function createIntern() {
          }
       });
 }
+
+
 
 
 // //menu()
